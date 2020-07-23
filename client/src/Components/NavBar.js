bf3b089f379767ccfb0247c18100c4d16a841712
Anthony Glover <antmajig@gmail.com>
2020-07-23 16:11:48 +0100

    did some css work on the navbar

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import '../nav.css'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "../nav.css";
+import { Link } from "react-router-dom";
 function NavBar() {
-    return (
-        <div>
-            <nav className="navBar">
-                <li>DuttysDen</li>
-                <Link to="/playerlist">
-                    <li>Players</li>
-                </Link>
-                <Link to="leaderboard">
-                    <li>Leaderboard</li>
-                </Link>
-            </nav>
-        </div>
-    );
+  return (
+    <div>
+      <nav className="navBar">
+        <li>Duttys Den</li>
+        <Link to="/playerlist">
+          <li className="nav-link">Players</li>
+        </Link>
+        <Link to="leaderboard">
+          <li className="nav-link">Leaderboard</li>
+        </Link>
+      </nav>
+    </div>
+  );
 }
 export default NavBar;
