5a8dc30b7f8881d9cab057339e09ed236592b6a9
Anthony Glover <antmajig@gmail.com>
2020-07-23 16:15:25 +0100

    Added leaderboard css

--- client/src/Components/Leaderboard.js
+++ client/src/Components/Leaderboard.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Table } from "react-bootstrap";
-
+import "../leaderboard.css";
 class Leaderboard extends Component {
   constructor(props) {
     super(props);
