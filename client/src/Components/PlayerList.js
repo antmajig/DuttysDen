cb44a5fd50923882fa06024c6f1eaa631a6dedf3
Anthony Glover <antmajig@gmail.com>
2020-07-23 23:29:38 +0100

    Player list results are now stored inside a table

--- client/src/Components/PlayerList.js
+++ client/src/Components/PlayerList.js
@@ -1,46 +1,59 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import Table from "react-bootstrap/Table";
 class PlayerList extends Component {
-    constructor() {
-        super();
-        this.state = {
-            error: null,
-            isLoaded: false,
-            items: []
-        };
-    }
+  constructor() {
+    super();
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+    };
+  }
 
-    getSQLResults() {
-        fetch("/playerlist")
-            .then(res => res.json())
-            .then(data => {
-                this.setState({ items: data, isLoaded: true })
-                console.log(this.state.items)
-            });
-    }
+  getSQLResults() {
+    fetch("/playerlist")
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ items: data, isLoaded: true });
+        console.log(this.state.items);
+      });
+  }
 
-    componentDidMount() {
-        this.getSQLResults();
-    }
+  componentDidMount() {
+    this.getSQLResults();
+  }
 
-    render() {
-        const { error, isLoaded, items } = this.state;
-        return (
-            <div className="PlayerList">
-                <h1>Players</h1>
-                {isLoaded ? (
-                    < div >
-                        {items.map(item =>
-                            <li id={item.PlayerID}>{item.PlayerName}</li>
-                        )}
-                    </div>
-                )
-                    :
-                    (<h3>Loading...</h3>)
-                }
-            </div>
-        );
-    }
+  render() {
+    const { error, isLoaded, items } = this.state;
+    return (
+      <div className="leaderboard">
+        {isLoaded ? (
+          <div>
+            <Table striped bordered hover size="sm" variant="dark">
+              <thead>
+                <tr>
+                  <th>Player Name</th>
+                  <th>Real Name</th>
+                  <th>Location</th>
+                </tr>
+              </thead>
+              <tbody>
+                {items.map((item) => (
+                  <tr>
+                    <td>{item.PlayerName}</td>
+                    <td>{item.RealName}</td>
+                    <td>{item.Location}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </div>
+        ) : (
+          <h3>Loading...</h3>
+        )}
+      </div>
+    );
+  }
 }
 
 export default PlayerList;
