990fc613b45b7b03363cb111432dfe0191aef5fa
Anthony Glover <antmajig@gmail.com>
2020-08-06 20:49:36 +0100

    change back to old proxy

--- src/setupProxy.js
+++
@@ -1,6 +0,0 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
-const proxy = require("http-proxy-middleware");
-
-module.exports = (app) => {
-  app.use(createProxyMiddleware("/*", { target: "http://localhost:9000/" }));
-};
