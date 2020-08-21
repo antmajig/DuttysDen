c7028cb307b4479757957f432bc0c19d3df94422
Anthony Glover <antmajig@gmail.com>
2020-08-21 14:57:35 +0100

    Now have background color for the site

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,20 +3,18 @@ import "../nav.css";
 import { Link } from "react-router-dom";
 function NavBar() {
   return (
-    <div>
-      <nav className="navBar">
-        <li>Dutty's Den</li>
-        <Link to="/playerlist">
-          <li className="nav-link">Players</li>
-        </Link>
-        <Link to="/leaderboard">
-          <li className="nav-link">Leaderboard</li>
-        </Link>
-        <Link to="/games">
-          <li className="nav-link">Games</li>
-        </Link>
-      </nav>
-    </div>
+    <nav className="navBar">
+      <li>Dutty's Den</li>
+      <Link to="/playerlist">
+        <li className="nav-link">Players</li>
+      </Link>
+      <Link to="/leaderboard">
+        <li className="nav-link">Leaderboard</li>
+      </Link>
+      <Link to="/games">
+        <li className="nav-link">Games</li>
+      </Link>
+    </nav>
   );
 }
 export default NavBar;
