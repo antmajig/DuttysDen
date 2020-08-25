18f646f7c6464e32f1b5a923ec68ebd9da53d6b3
Anthony Glover <antmajig@gmail.com>
2020-08-25 23:33:34 +0100

    fat seal carousel

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 import { Component } from "react";
-
+import Carousel from "react-bootstrap/Carousel";
+import "../index.css";
 class HomePage extends Component {
   render() {
-    return <h1>Homepage</h1>;
+    return (
+      <div className="caroselContainer">
+        <Carousel>
+          <Carousel.Item>
+            <img className="d-block" src="fatseal1.jpg" alt="First slide" />
+            <Carousel.Caption>
+              <h3>Welcome to Dutty's Den</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block" src="fatseal3.jpeg" alt="Third slide" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block" src="fatseal2.jpg" alt="Third slide" />
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    );
   }
 }
 export default HomePage;
