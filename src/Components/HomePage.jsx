b87ea123bc7ae4bfcc394d1d2c91e9d008581e64
Anthony Glover <antmajig@gmail.com>
2020-11-09 23:47:48 +0000

    Added particle effect to homepage

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,25 +1,19 @@
 import React from "react";
 import { Component } from "react";
-import Carousel from "react-bootstrap/Carousel";
+import BackgroundParticles from "./BackgroundParticles.jsx"
 import "../index.css";
 class HomePage extends Component {
   render() {
     return (
-      <div className="caroselContainer">
-        <Carousel>
-          <Carousel.Item>
-            <img className="d-block" src="fatseal1.jpg" alt="First slide" />
-            <Carousel.Caption>
-              <h3>Welcome to Dutty's Den</h3>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img className="d-block" src="fatseal3.jpeg" alt="Third slide" />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img className="d-block" src="fatseal2.jpg" alt="Third slide" />
-          </Carousel.Item>
-        </Carousel>
+      <div id="main">
+        <div id="backgroundParticles">       
+           <BackgroundParticles/>
+        </div>
+        <div id="center">
+          <div id="homePageImage">
+            <img src="fatseal1.jpg"></img>
+          </div> 
+        </div>
       </div>
     );
   }
